26696a2bbc353f0e3e429c3dcc7b55b43ec65047
xujinhan-web <xujinhan@fellou.ai>
2026-06-15 14:56:03 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.cdn1.vip',
+      },
+      {
+        protocol: 'https',
+        hostname: 's41.ax1x.com',
+      },
+    ],
   },
 };
 
